6e4d7cc628824a3299cd420d3a984e366a38dbc7
bmhsp <bayu0735@gmail.com>
2021-11-25 13:34:16 +0700

    testing api on  movies.show

--- tests/Feature/MoviesTest.php
+++ tests/Feature/MoviesTest.php
@@ -29,6 +29,24 @@ class MoviesTest extends TestCase
     $response->assertSee('upcoming');
   }
 
+  /** @test */
+  public function movie()
+  {
+    Http::fake([
+      'https://api.themoviedb.org/3/movie/*' => $this->fakeSingleMovie(),
+      'https://api.themoviedb.org/3/movie/*/recommendations' => $this->fakeRecommendations(),
+      'https://api.themoviedb.org/3/movie/*/keywords' => $this->fakeKeywords(),
+    ]);
+
+    $response = $this->get('/movies/566525/shang-chi-and-the-legend-of-the-ten-rings0');
+
+    $response->assertSee('Shang-Chi and the Legend of the Ten Rings');
+    $response->assertSee('Cast');
+    $response->assertSee('Info');
+    $response->assertSee('Images');
+    $response->assertSee('Recomended');
+  }
+
   private function fakeNowPlaying()
   {
     return Http::response([
@@ -172,6 +190,110 @@ class MoviesTest extends TestCase
         [
           "id" => 37,
           "name" => "Western"
+        ]
+      ]
+    ], 200);
+  }
+
+  private function fakeSingleMovie()
+  {
+    return Http::response([
+      "backdrop_path" => "/cinER0ESG0eJ49kXlExM0MEWGxW.jpg",
+      "belongs_to_collection" => null,
+      "budget" => 150000000,
+      "genres" => [
+        [
+          "id" => 28,
+          "name" => "Action"
+        ],
+        [
+          "id" => 12,
+          "name" => "Adventure"
+        ],
+        [
+          "id" => 14,
+          "name" => "Fantasy"
+        ],
+      ],
+      "homepage" => "https =>//www.marvel.com/movies/shang-chi-and-the-legend-of-the-ten-rings",
+      "id" => 566525,
+      "original_title" => "Shang-Chi and the Legend of the Ten Rings",
+      "overview" => "Shang-Chi must confront the past he thought he left behind when he is drawn into the web of the mysterious Ten Rings organization.",
+      "poster_path" => "/1BIoJGKbXjdFDAqUEiA2VHqkK1Z.jpg",
+      "production_companies" => [
+        [
+          "id" => 420,
+          "logo_path" => "/hUzeosd33nzE5MCNsZxCGEKTXaQ.png",
+          "name" => "Marvel Studios",
+          "origin_country" => "US"
+        ]
+      ],
+      "production_countries" => [
+        [
+          "iso_3166_1" => "US",
+          "name" => "United States of America"
+        ]
+      ],
+      "release_date" => "2021-09-01",
+      "revenue" => 430238384,
+      "runtime" => 132,
+      "status" => "Released",
+      "tagline" => "You can't outrun your destiny.",
+      "title" => "Shang-Chi and the Legend of the Ten Rings",
+      "video" => false,
+      "vote_average" => 7.9,
+    ], 200);
+  }
+
+  private function fakeRecommendations()
+  {
+    return Http::response([
+      'results' => [
+        [
+          "backdrop_path" => "/oE6bhqqVFyIECtBzqIuvh6JdaB5.jpg",
+          "id" => 522402,
+          "media_type" => "movie",
+          "title" => "Finch",
+          "original_title" => "Finch",
+          "poster_path" => "/jKuDyqx7jrjiR9cDzB5pxzhJAdv.jpg",
+          "release_date" => "2021-11-04",
+          "vote_average" => 8.245,
+        ]
+      ]
+    ], 200);
+  }
+
+  private function fakeKeywords()
+  {
+    return Http::response([
+      'keywords' => [
+        [
+          "id" => 779,
+          "name" => "martial arts"
+        ],
+        [
+          "id" => 9715,
+          "name" => "superhero"
+        ],
+        [
+          "id" => 9717,
+          "name" => "based on comic"
+        ],
+        [
+          "id" => 9917,
+          "name" => "mixed martial arts"
+        ],
+        [
+          "id" => 179431,
+          "name" => "duringcreditsstinger"
+        ],
+        [
+          "id" => 180547,
+          "name" => "marvel cinematic universe (mcu)"
+        ],
+        [
+          "id" => 265894,
+          "name" => "marvel comics"
         ]
       ]
     ], 200);
